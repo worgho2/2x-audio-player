d550c9abe70b2c8f2d2e5468391dec2adba58b6d
Bruno Pastre <pastre68@gmail.com>
2020-11-15 22:48:21 -0300

    Properly configures tabbar

--- 2xSpeedAudio/Main/TXTabBarController.swift
+++ 2xSpeedAudio/Main/TXTabBarController.swift
@@ -2,17 +2,41 @@ import UIKit
 
 final class TXTabBarController: UITabBarController {
     
+    // MARK: - Inner types
+    struct TabBarItem {
+        let title: String
+        let image: UIImage
+        let viewController: UIViewController
+    }
+    
     // MARK: - Initalization
     init() {
         super.init(nibName: nil, bundle: nil)
-        let url = Bundle.main.url(forResource: "start", withExtension:"MP4")!
-        viewControllers = [
-            TXTutorialViewController(
-                url: url,
-                title: "Start",
-                description: "this is the fist time this is happening"
+        let url = Bundle.main.url(forResource: "demo", withExtension:"MP4")!
+        initTabBar(with: [
+            .init(
+                title: "Home",
+                image:UIImage(systemName: "house.fill")!,
+                viewController: TXTutorialViewController(
+                        url: url,
+                        title: "Start",
+                        description: "this is the fist time this is happening"
+                ))
+        ])
+    }
+    
+    private func initTabBar(with items: [TabBarItem]) {
+        
+        items.forEach { item in
+            let vc = item.viewController
+            vc.tabBarItem = UITabBarItem(
+                title: item.title,
+                image: item.image,
+                selectedImage: item.image
             )
-        ]
+        }
+        
+        viewControllers = items.map { $0.viewController }
     }
     
     // MARK: - Unused
